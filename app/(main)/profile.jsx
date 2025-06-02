1e9e223b728ee922431e688be2f002d9ea27bedc
Yusri Saad <yusryku@gmail.com>
2025-06-02 22:36:01 +0800

    fix logout

--- app/(main)/profile.jsx
+++ app/(main)/profile.jsx
@@ -25,6 +25,9 @@ const Profile = () => {
     const { error } = await supabase.auth.signOut();
     if (error) {
       Alert.alert("Logout", error.message);
+    } else {
+      setAuth(null);
+      router.replace("/welcome");
     }
   };
 
